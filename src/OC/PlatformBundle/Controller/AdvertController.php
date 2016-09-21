5c54f4edefbf71f7cbb7af4177dd3d8837010602
richterlaurent <richterlaurent@gmail.com>
2016-09-21 12:50:12 +0200

    Add Service : Antispam in OCPlatformBundle and direct use in addAction in the AdvertController

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -13,6 +13,7 @@ class AdvertController extends Controller
 {
     public function indexAction($page)
     {
+
         if($page < 1){
             throw new NotFoundHttpException('Page "'.$page.'" inexistante.');
         }else {
@@ -60,12 +61,20 @@ class AdvertController extends Controller
 
     public function addAction(Request $request)
     {
+
+        $antispam = $this->container->get('oc_platform.antispam');
+            $text = '...';
+
+        if($spam = $antispam->isSpam($text)){
+            throw new \Exception('Votre message a été détecté comme spam.');
+        }
+
         if($request->isMethod('POST')){
             $request->getSession()->getFlashBag()->add('info', 'Annonce bien enregistrée.');
             return $this->redirectToRoute('oc_platform_view', array('id' => 5));
         }
         // Si pas en post, on afficher le formulaire =>
-        return $this->render('OCPlatformBundle:Advert:add.html.twig');
+        return $this->render('OCPlatformBundle:Advert:add.html.twig', array());
     }
 
     public function editAction($id, Request $request){
